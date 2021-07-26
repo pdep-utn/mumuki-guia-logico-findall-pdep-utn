8b78cf54e17e1278821937c2aea12ac605b1ad09
julian@mumuki.org <julian@mumuki.org>
2021-07-26 21:32:22 +0000

    Mumuki Export on 2021-07-26 21:32:22 +0000

--- 00007_Usar findall y no morir en el intento/test.pl
+++ 00007_Usar findall y no morir en el intento/test.pl
@@ -1,2 +1,5 @@
 test('costoTotalDeHerramientasDemandadas(550) si se demanda un martillo de $500 y una pinza de $50') :-
    costoTotalDeHerramientasDemandadas(550).
+
+test('not(costoTotalDeHerramientasDemandadas(650)) si se demanda un martillo de $500 y una pinza de $50') :-
+   not(costoTotalDeHerramientasDemandadas(650)).
